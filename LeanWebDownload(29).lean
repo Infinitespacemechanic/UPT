e2bde7bee8ab7b1c031626bff9dc96bafc17e922
Infinitespacemechanic <roblaakkonen@gmail.com>
2026-09-24 17:09:54 -0400

    Delete LeanWebDownload(29).lean

--- LeanWebDownload(29).lean
+++
@@ -1,5 +0,0 @@
-def life_of_three : Bool := true
-def whole_unit := 3
-def parts := 1.0472 -- π/3
-def margin := 1.0/3.0 -- 1/3 margin keeps it alive
-def living : Bool := (1 + 1 + parts) > 2 -- 1+1>2
